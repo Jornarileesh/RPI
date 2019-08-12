24278291b7391b22254dd30331593af6e7339877
Jornarileesh <t.ciarachowicz@gmail.com>
2019-08-13 00:20:43 +0200

    led control

--- blynk.py
+++ blynk.py
@@ -1,12 +1,15 @@
 import blynklib
 import RPi.GPIO as GPIO
 
+
 BLYNK_AUTH = 'tLQHdWKu8j0ZyQ_C0H1L1nUVsWI5iKA8'
 # base lib init
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
+
+WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
 
 
 def turn_on_light():
@@ -17,30 +20,13 @@ def turn_off_light():
     GPIO.output(21, GPIO.LOW)
 
 
-@blynk.handle_event('read V22')
-def read_virtual_pin_handler(pin):
-    # your code goes here
-    # ...
-    # Example: get sensor value, perform calculations, etc
-    sensor_data = '<YourSensorData>'
-    critilcal_data_value = '<YourThresholdSensorValue>'
+@blynk.handle_event('write V4')
+def write_virtual_pin_handler(pin, value):
+    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
 
-    # send value to Virtual Pin and store it in Blynk Cloud
-    blynk.virtual_write(pin, sensor_data)
 
-    # you can define if needed any other pin
-    # example: blynk.virtual_write(24, sensor_data)
-
-    # you can perform actions if value reaches a threshold (e.g. some critical value)
-    if sensor_data >= critilcal_data_value:
-
-        blynk.set_property(pin, 'color', '#FF0000')  # set red color for the widget UI element
-        blynk.notify('Warning critical value')  # send push notification to Blynk App
-        # blynk.email( < youremail @ email.com >, 'Email Subject', 'Email Body')  # send email to specified address
-
-        # main loop that starts program and handles registered events
-        while True:
-            blynk.run()
+while True:
+    blynk.run()
 
 
 # dioda - pin 40 (GPIO21)
