23f7392b08ba6c3d73867a61655d513bc84594b0
Jornarileesh <t.ciarachowicz@gmail.com>
2019-08-13 01:08:24 +0200

    maybe now

--- blynk.py
+++ blynk.py
@@ -7,9 +7,7 @@ BLYNK_AUTH = 'tLQHdWKu8j0ZyQ_C0H1L1nUVsWI5iKA8'
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
 GPIO.setmode(GPIO.BCM)
-# GPIO.setup(21, GPIO.OUT)
-
-WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
+GPIO.setup(21, GPIO.OUT)
 
 
 def turn_on_light():
@@ -20,14 +18,13 @@ def turn_off_light():
     GPIO.output(21, GPIO.LOW)
 
 
-pin = 'GP21'
+pin = GPIO.setup(21, GPIO.OUT)
 value = 'HIGH'
 
 
 @blynk.handle_event('LED ON')
-def write_virtual_pin_handler(pin, value):
-    blynk.virtual_write(pin, value)
-    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+def write_virtual_pin_handler():
+    turn_on_light()
 
 
 while True:
