32538293f3e127e0dd0aed64f5db6e7c9d66001a
Jornarileesh <t.ciarachowicz@gmail.com>
2019-08-19 23:32:03 +0200

    this is work

--- blynk.py
+++ blynk.py
@@ -1,36 +1,63 @@
 import blynklib
+# import blynktimer
 import RPi.GPIO as GPIO
+import Adafruit_DHT
+import time
+from datetime import datetime
 
+BLYNK_AUTH = 'xxx'
+# blynk = blynklib.Blynk(BLYNK_AUTH)
 
-BLYNK_AUTH = 'tLQHdWKu8j0ZyQ_C0H1L1nUVsWI5iKA8'
-# base lib init
-blynk = blynklib.Blynk(BLYNK_AUTH)
+blynk = blynklib.Blynk(BLYNK_AUTH,
+                       server='blynk-cloud.com',
+                       port=8442,
+                       heartbeat=60
+                       )
+
+# timer = blynktimer.Timer()
+
+WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_0_EVENT] Pin: V{} Value: '{}'"
+WRITE_EVENT_PRINT_MSG2 = "[WRITE_VIRTUAL_PIN_1_EVENT] Pin: V{} Value: '{}'"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
-turn_off_light():
+GPIO.setup(4, GPIO.OUT)
+
+GPIO.output(21, GPIO.LOW)
+time.sleep(1)
+GPIO.output(21, GPIO.HIGH)
+time.sleep(1)
+GPIO.output(21, GPIO.LOW)
+
+dht11_sensor = Adafruit_DHT.DHT11
 
 
-def turn_on_light():
-    GPIO.output(21, GPIO.HIGH)
-
-
-def turn_off_light():
-    GPIO.output(21, GPIO.LOW)
-
-
-@blynk.handle_event('write GP21')
+@blynk.handle_event('write V0')
 def write_virtual_pin_handler(pin, value):
-    if value == 1:
-        blynk.virtual_write(21, 1)
-        # turn_on_light()
-    elif value == 0:
-        blynk.virtual_write(21, 0)
-        # turn_off_light()
+    # blynk.virtual_write(1, 1)
+    print(WRITE_EVENT_PRINT_MSG.format(pin, value[0]))
+    if value[0] == '1':
+        GPIO.output(21, GPIO.HIGH)
+        print(1)
+    else:
+        GPIO.output(21, GPIO.LOW)
+        print(0)
+
+
+@blynk.handle_event('V2')
+def write_temp_humid():
+    humidity, temperature = Adafruit_DHT.read_retry(dht11_sensor, 4, retries=10, delay_seconds=10)
+    if None not in (humidity, temperature):
+        # print('{}: {:.1f} C, {:.1f} %'.format(datetime.now(), temperature, humidity))
+        blynk.virtual_write(2, humidity)
+        blynk.virtual_write(3, temperature)
+    else:
+        blynk.virtual_write(2, 0)
+        blynk.virtual_write(3, 0)
+        print('None error')
 
 
 while True:
     blynk.run()
+    write_temp_humid()
 
-# dioda - pin 40 (GPIO21)
-# masa pin 39
